8020fde3d471dd3ea816c5285218c1cdd4395a62
bit <bit@DESKTOP-08RGQCM>
2018-03-22 16:17:05 +0900

    change

--- HelloGit/src/HelloGit.java
+++ HelloGit/src/HelloGit.java
@@ -3,14 +3,15 @@ public class HelloGit {
 	// https://github.com/jsh2174/setEclipse.git
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
-		System.out.println("1_Hello_Git"); 
-		// 1.hotfix 추가 후 마스터 브랜치 수정  
-		System.out.println("2_add hotfix");
 		// 2. merge 후 마스터 브랜치 수정 !! 
 		System.out.println("3_add master ");
 		// 3. github updata 
 		System.out.println("4_github update master ");
+		// TODO Auto-generated method stub
+		System.out.println("1_Hello_Git"); 
+		// 1.hotfix 추가 후 마스터 브랜치 수정  
+		System.out.println("2_add hotfix");
+	
 	
 		
 	}
